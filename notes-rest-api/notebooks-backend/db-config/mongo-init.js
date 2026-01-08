9cf2b560ad2d807697732f359e6d50ce8de045e1
Jeff Mbiada <jeff@itasapro.com>
2026-01-08 22:50:42 +0100

    update notes rest api

---
+++ notes-rest-api/notebooks-backend/db-config/mongo-init.js
@@ -0,0 +1,12 @@
+const notebooksDbUser = process.env.NOTEBOOKS_DB_USER;
+const notebooksDbPassword = process.env.NOTEBOOKS_DB_PASSWORD;
+const notebooksDbName = process.env.NOTEBOOKS_DB_NAME;
+
+console.log('INITIALIZING DB USER');
+db = db.getSiblingDB(notebooksDbName);
+
+db.createUser({
+  user: notebooksDbUser,
+  pwd: notebooksDbPassword,
+  roles: [{ role: "readWrite", db: notebooksDbName }],
+});
